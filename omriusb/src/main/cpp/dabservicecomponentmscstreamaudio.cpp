realzoulou/omri-usb

commit 2c7b2fce21ba280ba4c1f1e20abe206a3457e793

Add JDabService::unlinkDabService() as opposite to setLinkDabService()
Maybe it is useful to use std::shared_ptr<>::reset() before setting the shared_ptr to nullptr

omriusb/src/main/cpp/platformspecific/android/jdabservice.cpp
@@ -116,8 +116,34 @@ JDabService::~JDabService() {
     if(wasDetached) {
         m_javaVm->DetachCurrentThread();
     }
+}
 
-    m_audioDataCb = nullptr;
+void JDabService::unlinkDabService() {
+    std::lock_guard<std::recursive_mutex> lockGuard(m_mutex);
+    std::cout << m_logTag << "Un-Linking DABServices... for SId " << std::hex << m_serviceId << std::endl;
+    if (m_linkedDabService != nullptr) {
+        for (const auto& srvComp : m_linkedDabService->getServiceComponents()) {
+            if (srvComp->getServiceComponentType() == DabServiceComponent::SERVICECOMPONENTTYPE::MSC_STREAM_AUDIO) {
+                std::shared_ptr<DabServiceComponentMscStreamAudio> audioComponent = std::static_pointer_cast<DabServiceComponentMscStreamAudio>(srvComp);
+                audioComponent->clearCallbacks();
+                // TODO clear callbacks in getUserApplications
+            }
+        }
+        m_linkedDabService.reset();
+        m_linkedDabService = nullptr;
+    }
+    if (m_audioDataCb != nullptr) {
+        m_audioDataCb.reset();
+        m_audioDataCb = nullptr;
+    }
+    if (m_dlsCallback != nullptr) {
+        m_dlsCallback.reset();
+        m_dlsCallback = nullptr;
+    }
+    if (m_slsCallback != nullptr) {
+        m_slsCallback.reset();
+        m_slsCallback = nullptr;
+    }
 }
 
 void JDabService::setLinkDabService(std::shared_ptr<DabService> linkedDabSrv) {

omriusb/src/main/cpp/platformspecific/android/jdabservice.h
@@ -36,6 +36,7 @@ public:
     virtual ~JDabService();
 
     virtual void setLinkDabService(std::shared_ptr<DabService> linkedDabSrv);
+    virtual void unlinkDabService();
 
     virtual jobject getJavaDabServiceObject() const;
     virtual std::shared_ptr<DabService> getLinkDabService() const;

omriusb/src/main/cpp/platformspecific/android/raontunerinput.cpp
@@ -172,11 +172,13 @@ void RaonTunerInput::startServiceSync(std::shared_ptr<JDabService> serviceLink) 
 
     if(m_startServiceLink != nullptr) {
         m_startServiceLink->decodeAudio(false);
+        m_startServiceLink->unlinkDabService();
         if(m_startServiceLink->getJavaDabServiceObject() != nullptr) {
             if (m_usbDevice != nullptr) {
                 m_usbDevice->serviceStopped(m_startServiceLink->getJavaDabServiceObject());
             }
         }
+        m_startServiceLink.reset();
     }
 
     if(m_currentFrequency != serviceLink.get()->getEnsembleFrequency()) {
@@ -399,7 +401,8 @@ void RaonTunerInput::setService() {
                 }
 
                 m_startServiceLink->decodeAudio(true);
-                if (m_usbDevice != nullptr) {
+                if (m_usbDevice != nullptr &&
+                    m_startServiceLink->getJavaDabServiceObject() != nullptr) {
                     m_usbDevice->serviceStarted(m_startServiceLink->getJavaDabServiceObject());
                 }
                 break;
@@ -1113,12 +1116,14 @@ void RaonTunerInput::closeSubchannel(uint8_t subchanId) {
 
     if(m_startServiceLink != nullptr) {
         m_startServiceLink->decodeAudio(false);
+        m_startServiceLink->unlinkDabService();
         if(m_startServiceLink->getJavaDabServiceObject() != nullptr) {
             if (m_usbDevice != nullptr) {
                 m_usbDevice->serviceStopped(m_startServiceLink->getJavaDabServiceObject());
             }
         }
 
+        m_startServiceLink.reset();
         m_startServiceLink = nullptr;
     }
 }

commit 41ce207855311434ef2f5d8a19289f8203431889

Fix: When switching between stations on same ensemble frequency, DLS text and MOT slideshow picture are not updated anymore
regression introduced with [2c7b2fc] Add JDabService::unlinkDabService() as opposite to setLinkDabService()

omriusb/src/main/cpp/dabservicecomponentmscstreamaudio.cpp
@@ -131,8 +131,8 @@ std::shared_ptr<DabServiceComponentMscStreamAudio::AUDIO_DATA_CALLBACK> DabServi
 
 void DabServiceComponentMscStreamAudio::clearCallbacks() {
     if(m_ascTy == 0x3F) {
-        return (std::static_pointer_cast<DabPlusServiceComponentDecoder>(m_componentDecoder))->clearCallbacks();
+        (std::static_pointer_cast<DabPlusServiceComponentDecoder>(m_componentDecoder))->clearCallbacks();
     } else {
-        return (std::static_pointer_cast<DabMpegServiceComponentDecoder>(m_componentDecoder))->clearCallbacks();
+        (std::static_pointer_cast<DabMpegServiceComponentDecoder>(m_componentDecoder))->clearCallbacks();
     }
 }

omriusb/src/main/cpp/platformspecific/android/jdabservice.cpp
@@ -122,13 +122,6 @@ void JDabService::unlinkDabService() {
     std::lock_guard<std::recursive_mutex> lockGuard(m_mutex);
     std::cout << m_logTag << "Un-Linking DABServices... for SId " << std::hex << m_serviceId << std::endl;
     if (m_linkedDabService != nullptr) {
-        for (const auto& srvComp : m_linkedDabService->getServiceComponents()) {
-            if (srvComp->getServiceComponentType() == DabServiceComponent::SERVICECOMPONENTTYPE::MSC_STREAM_AUDIO) {
-                std::shared_ptr<DabServiceComponentMscStreamAudio> audioComponent = std::static_pointer_cast<DabServiceComponentMscStreamAudio>(srvComp);
-                audioComponent->clearCallbacks();
-                // TODO clear callbacks in getUserApplications
-            }
-        }
         m_linkedDabService.reset();
         m_linkedDabService = nullptr;
     }