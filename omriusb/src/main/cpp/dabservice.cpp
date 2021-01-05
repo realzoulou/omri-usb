realzoulou/omri-usb

commit d2cdcd067bf618ab52a04e41ac29025eb0ee8cfb

Fix: MOT SLS not shown because FIG 0/13 is too early considered as done. Solution: expect every PS to have Dls and MotSls User Applications
* Introduced with [21741d1]

omriusb/src/main/cpp/dabservice.cpp
@@ -171,34 +171,34 @@ DabEnsemble* DabService::getDabEnsemble() const {
 bool DabService::checkSanity() const {
     bool isSane = true;
     std::stringstream logStr;
-    logStr << m_logTag << "check sanity SId=0x" << std::hex << +getServiceId() << std::dec << ":";
+    logStr << m_logTag << "check sanity SId=0x" << std::hex << +getServiceId() << std::dec << ": ";
 
     if (getServiceId() == SID_INVALID) {
-        logStr << " SID:invalid";
+        logStr << "SID:invalid";
         isSane = false;
     }
     else if (getEnsembleFrequency() == DabEnsemble::FREQ_INVALID) {
-        logStr << " EFreq:invalid";
+        logStr << "EFreq:invalid";
         isSane = false;
     }
     else if (getNumberServiceComponents() == 0) {
-        logStr << " numCmp:0";
+        logStr << "numCmp:0";
         isSane = false;
     }
     else if (getLabelCharset() == CHARSET_INVALID) {
-        logStr << " charset:invalid";
+        logStr << "charset:invalid";
         isSane = false;
     }
     else if (getServiceLabel().empty()) {
-        logStr << " label:empty";
+        logStr << "label:empty";
         isSane = false;
     }
     else if (getServiceShortLabel().empty()) {
-        logStr << " short label:empty";
+        logStr << "short label:empty";
         isSane = false;
     }
     else if (getServiceComponents().size() != getNumberServiceComponents()) {
-        logStr << " components != numCmp:" << +getServiceComponents().size() << "/" << +getNumberServiceComponents();
+        logStr << "components != numCmp:" << +getServiceComponents().size() << "/" << +getNumberServiceComponents();
         isSane = false;
     } else {
         for (const auto& srvCmp : getServiceComponents()) {
@@ -209,6 +209,33 @@ bool DabService::checkSanity() const {
                 isSane = false;
                 break;
             }
+        }
+    }
+    if (isSane && isProgrammeService()) {
+        bool hasDlsUserApp = false, hasMotSlsUserApp = false;
+        for (const auto &srvComp : getServiceComponents()) {
+            if (srvComp->getServiceComponentType() ==
+                DabServiceComponent::SERVICECOMPONENTTYPE::MSC_STREAM_AUDIO &&
+                (srvComp->isPrimary() || getNumberServiceComponents() == 1)) {
+                for (const auto &uApp : srvComp->getUserApplications()) {
+                    switch (uApp.getUserApplicationType()) {
+                        case registeredtables::USERAPPLICATIONTYPE::DYNAMIC_LABEL:
+                            hasDlsUserApp = true;
+                            break;
+                        case registeredtables::USERAPPLICATIONTYPE::MOT_SLIDESHOW:
+                            hasMotSlsUserApp = true;
+                            break;
+                        default: // don't care
+                            break;
+                    }
+                }
+            }
+        }
+        // Programme Service must have at least these two User Applications
+        // maybe dangerous to assume this? No DAB spec reference found
+        if (! (hasDlsUserApp && hasMotSlsUserApp)) {
+            logStr << "PS but DlsUserApp=" << std::boolalpha << hasDlsUserApp << ", MotSlsUserApp=" << hasMotSlsUserApp << std::noboolalpha;
+            isSane = false;
         }
     }
     if (!isSane && logStr.str().length() > 0) {

commit 65c11c08a85c706f561355b29433c8f5f586092a

Fix wrong assumption introduced in [d2cdcd0], bring back the done-detection for FIG 0/8 and 0/13

omriusb/src/main/cpp/dabservice.cpp
@@ -211,33 +211,6 @@ bool DabService::checkSanity() const {
             }
         }
     }
-    if (isSane && isProgrammeService()) {
-        bool hasDlsUserApp = false, hasMotSlsUserApp = false;
-        for (const auto &srvComp : getServiceComponents()) {
-            if (srvComp->getServiceComponentType() ==
-                DabServiceComponent::SERVICECOMPONENTTYPE::MSC_STREAM_AUDIO &&
-                (srvComp->isPrimary() || getNumberServiceComponents() == 1)) {
-                for (const auto &uApp : srvComp->getUserApplications()) {
-                    switch (uApp.getUserApplicationType()) {
-                        case registeredtables::USERAPPLICATIONTYPE::DYNAMIC_LABEL:
-                            hasDlsUserApp = true;
-                            break;
-                        case registeredtables::USERAPPLICATIONTYPE::MOT_SLIDESHOW:
-                            hasMotSlsUserApp = true;
-                            break;
-                        default: // don't care
-                            break;
-                    }
-                }
-            }
-        }
-        // Programme Service must have at least these two User Applications
-        // maybe dangerous to assume this? No DAB spec reference found
-        if (! (hasDlsUserApp && hasMotSlsUserApp)) {
-            logStr << "PS but DlsUserApp=" << std::boolalpha << hasDlsUserApp << ", MotSlsUserApp=" << hasMotSlsUserApp << std::noboolalpha;
-            isSane = false;
-        }
-    }
     if (!isSane && logStr.str().length() > 0) {
         std::cout << logStr.str() << std::endl;
     }

omriusb/src/main/cpp/ficparser.cpp
@@ -210,7 +210,17 @@ void FicParser::parseFig_00(const std::vector<uint8_t>& ficData) {
         case Fig::FIG_00_TYPE::SERVICE_COMPONENT_GLOBAL_DEFINITION: {
             Fig_00_Ext_08 extEight(ficData);
             m_fig00_08dispatcher.invoke(extEight);
-            m_fig00DoneDispatcher.invoke(Fig::FIG_00_TYPE::SERVICE_COMPONENT_GLOBAL_DEFINITION);
+
+            bool done{false};
+            if(!contains<Fig_00_Ext_08>(m_parsedFig0008, extEight)) {
+                m_parsedFig0008.push_back(extEight);
+            } else {
+                done = true;
+            }
+
+            if (done) {
+                m_fig00DoneDispatcher.invoke(Fig::FIG_00_TYPE::SERVICE_COMPONENT_GLOBAL_DEFINITION);
+            }
             break;
         }
         case Fig::FIG_00_TYPE::COUNTRY_LTO_INTERNATIONAL_TABLE: {
@@ -226,7 +236,17 @@ void FicParser::parseFig_00(const std::vector<uint8_t>& ficData) {
         case Fig::FIG_00_TYPE::USERAPPLICATION_INFORMATION: {
             Fig_00_Ext_13 ext3Ten(ficData);
             m_fig00_13dispatcher.invoke(ext3Ten);
-            m_fig00DoneDispatcher.invoke(Fig::FIG_00_TYPE::USERAPPLICATION_INFORMATION);
+
+            bool done{false};
+            if(!contains<Fig_00_Ext_13>(m_parsedFig0013, ext3Ten)) {
+                m_parsedFig0013.push_back(ext3Ten);
+            } else {
+                done = true;
+            }
+
+            if (done) {
+                m_fig00DoneDispatcher.invoke(Fig::FIG_00_TYPE::USERAPPLICATION_INFORMATION);
+            }
             break;
         }
         case Fig::FIG_00_TYPE::FEC_SUBCHANNEL_ORGANIZATION: {

omriusb/src/main/cpp/ficparser.h
@@ -220,6 +220,11 @@ private:
     bool FIB_CRC_CHECK(const uint8_t* data) const;
 
 private:
+    // FIG 0/8 and 0/13 are signalled very slowly (at least every 1s)
+    std::vector<Fig_00_Ext_08> m_parsedFig0008;
+    std::vector<Fig_00_Ext_13> m_parsedFig0013;
+
+private:
     template<class T>
     bool contains(std::vector<T> a, T b) {
         for(const auto& fig : a) {

omriusb/src/main/cpp/fig_00_ext_08.h
@@ -66,12 +66,13 @@ public:
         if(m_globalDefinitions.size() != other.m_globalDefinitions.size()) {
             return false;
         }
-        for(int i = 0; i < m_globalDefinitions.size(); i++) {
-            if(m_globalDefinitions[i] != other.m_globalDefinitions[i]) {
+        // don't assume that the vectors have the same sorting
+        for (const auto & otherGlobalDefinition: other.m_globalDefinitions) {
+            if (std::find(m_globalDefinitions.begin(), m_globalDefinitions.end(),
+                          otherGlobalDefinition) == m_globalDefinitions.end()) {
                 return false;
             }
         }
-
         return true;
     }
     inline bool operator!=(const Fig_00_Ext_08& other) const { return !operator==(other); }

omriusb/src/main/cpp/fig_00_ext_13.h
@@ -101,12 +101,13 @@ public:
         if(m_userAppDescriptions.size() != other.m_userAppDescriptions.size()) {
             return false;
         }
-        for(int i = 0; i < m_userAppDescriptions.size(); i++) {
-            if(m_userAppDescriptions[i] != other.m_userAppDescriptions[i]) {
+        // don't assume that the vectors have the same sorting
+        for (const auto & otherUserApp : other.m_userAppDescriptions) {
+            if (std::find(m_userAppDescriptions.begin(), m_userAppDescriptions.end(),
+                    otherUserApp) == m_userAppDescriptions.end()) {
                 return false;
             }
         }
-
         return true;
     }
     inline bool operator!=(const Fig_00_Ext_13& other) const { return !operator==(other); }